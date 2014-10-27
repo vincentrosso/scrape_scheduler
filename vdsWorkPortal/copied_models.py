9959be9c64edc0a20c69d60f6312104b27b4b76f
sogdahl <inC4Rnate>
2014-10-27 18:22:01 -0500

    Updated models

--- vdsWorkPortal/copied_models.py
+++ vdsWorkPortal/copied_models.py
@@ -1,14 +1,22 @@
 __author__ = 'Steven Ogdahl'
 
 from datetime import datetime
+import json
+import requests
+import socket
 from time import strftime
+from urllib2 import urlopen
 
+from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils.http import urlencode
+from django.utils.timezone import now as utcnow
 
-import vdsClientPortal.enums
+from vdsClientPortal.enums import SystemAudit_types
 import vdsWorkPortal.common.utils
 import vdsWorkPortal.common.enums
+
 
 class ScrapeSource(models.Model):
     from titleapi.copied_models import County
@@ -19,68 +27,60 @@ class ScrapeSource(models.Model):
     type = models.TextField()
     is_enabled = models.BooleanField(default=True, blank=False, null=False, verbose_name="IsEnabled")
 
-    def get_image_scrape_url(self, token="",  AFN="", book="", page="", recdate="", scrape_type="Image", county=None, contract_name=None, pages=None, property_type=""):
-        try:
-            scrape = None
-            url = ""
-            extra = ""
-            if contract_name != None:
-                scrape = ScrapeSource.objects.get(is_enabled=True, contract_name=contract_name)
-            else:
-                doctype = "docid"
-                if book !="" and page != "":
-                    doctype = "book"
-                if pages is not None:
-                    doctype = "fetcher"
-                scrapeList = ScrapeSource.objects.filter(
-                    is_enabled=True,
-                    county=county,
-                    type=scrape_type,
-                    contract_name__contains=doctype)
-                if len(scrapeList) > 0:
-                    scrape = scrapeList[0]
-            if scrape != None:
-                recdateprased = vdsWorkPortal.common.utils.dateparse(recdate)
-                formattedDate = strftime("%m%d%Y", recdateprased)
-                d = dict()
-                d["url"] = scrape.url
-                d["token"] = token
-                d["docid"] = AFN
-                d["book"] = book
-                d["page"] = page
-                d["date"] = formattedDate
-                if property_type and len(property_type) > 0:
-                    d["property_type"] = property_type[:1]
-                if pages is not None:
-                    d["numpages"] = pages
-                try:
-                    d["fips"] = county.fips
-                except Exception:
-                    pass
-                url = scrape.format_url(d).replace(' ','%20')
-            return url
-        except Exception, err:
-            SystemAudit.add("exception in get_image_scrape_url {0}".format(err), vdsClientPortal.enums.SystemAudit_types.ROBOT_ACTION_ERROR)
-            return None
+    _readonly = False
+    formatted_url = None
+    response = None
 
-    def format_url(self, dictionary): # format, url, token=None, AFN=None, book=None, page=None, recdate=None):
+    def save(self, *args, **kwargs):
+        if self._readonly:
+            return
+        super(ScrapeSource, self).save(*args, **kwargs)
+
+    @staticmethod
+    def filter_by_county(county, *args, **kwargs):
+        ss_list = ScrapeSource.objects.filter(Q(county=county) | Q(county__isnull=True, state=county.state_short)).\
+            filter(*args, **kwargs).order_by('county__county_name')
+        # Set the county and "readonly" for each of the scrape sources.  The "readonly" bit is so the scrape source
+        # doesn't accidentally get overwritten (important for state-level scrapes)
+        for ss in ss_list:
+            ss.county = county
+            ss._readonly = True
+        return ss_list
+
+    def format_url(self, dictionary):
         url_format = self.urlFormat
         d = dictionary.copy()
         if "url" not in d:
             d['url'] = self.url
         if 'fips' not in d and self.county:
             d['fips'] = self.county.fips
-        url_format = url_format.format(**d)
+        if 'county' not in d and self.county:
+            d['county'] = self.county.county_name
         url_parts = url_format.split("?")
-        url_path = url_parts[0]
         url_query = ""
         if len(url_parts) > 1:
-            url_query = "?" + urlencode(dict( (n,v) for n,v in (a.split('=') for a in url_parts[1].split("&") ) )) #gotta love python!
+            url_query = "?" + urlencode([[y[0].format(**d), y[1].format(**d)] for y in [x.split('=') for x in url_parts[1].split('&')]])
 
-        return url_path + url_query
+        return url_parts[0].format(**d) + url_query
+
+    @staticmethod
+    def list_contracts_by_county(county):
+        available = []
+        scrapes = ScrapeSource.filter_by_county(county, is_enabled=True)
+        contracts = vdsWorkPortal.common.enums.scrape_contract_type().as_list()
+        for s in scrapes:
+            for c in contracts:
+                if c in s.contract_name:
+                    available.append((c, s.contract_name))
+        return available
 
     def __unicode__(self):
-        return u'[%s] %s %s' % (self.contract_name, self.county.county_name if self.county else 'None', self.type)
+        return u'[%s] %s %s' % (
+            self.contract_name,
+            self.state if self.state else
+                "{0}/{1}".format(self.county.state_short, self.county.county_name) if self.county else 'None',
+            self.type
+        )
 
     @property
     def display(self):
@@ -90,12 +90,40 @@ class ScrapeSource(models.Model):
             'urlFormat': self.urlFormat,
             'county': self.county,
             'type': self.type,
-            'is_enabled': self.is_enabled
+            'is_enabled': self.is_enabled,
+            'str': str(self)
         }
+
+    def execute(self, method=None, data=None, parameters=None, synchronous=False):
+        if parameters:
+            self.formatted_url = self.format_url(parameters)
+        else:
+            self.formatted_url = self.format_url({})
+
+        # Hack for now -- this could be a bit cleaner
+        if synchronous:
+            self.formatted_url = self.formatted_url.replace("/scrape/", "/scrape_sync/")
+
+        s_data = ""
+        try:
+            s_data = "data=None" if data is None else json.dumps(data)
+        except Exception:
+            pass
+        try:
+            if method is not None and "POST" in method:
+                self.response = urlopen(self.formatted_url, json.dumps(data))
+            else:
+                self.response = requests.get(self.formatted_url)
+            SystemAudit_add("scrape.execute {0} {1}".format(self.formatted_url, s_data), SystemAudit_types.ROBOT_ACTION)
+        except Exception, err:
+            SystemAudit_add("Error in scrape.execute scrape: {0} {1} error: {2}".format(self.formatted_url, s_data, err), SystemAudit_types.ROBOT_ACTION_ERROR)
+            print str(err)
+
+        return self.response
 
 
 class SystemAudit(models.Model):
-    auditTimeStamp = models.DateTimeField()
+    auditTimeStamp = models.DateTimeField(default=utcnow)
     auditMessage = models.TextField(default="")
     auditType = models.TextField(default="INFO")
 
@@ -103,7 +131,14 @@ class SystemAudit(models.Model):
     def add(msg, audit_type):
         sa = SystemAudit()
         sa.auditMessage = msg
-        sa.auditTimeStamp = datetime.now()
         sa.auditType = audit_type
         sa.save()
         return sa
+
+
+def SystemAudit_add(msg, audit_type):
+    sa = SystemAudit()
+    sa.auditMessage = msg
+    sa.auditType = audit_type
+    sa.save()
+    return sa
